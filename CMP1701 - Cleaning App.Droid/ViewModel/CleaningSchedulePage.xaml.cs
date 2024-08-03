b1b41130348e6dcf0e7327a7948769e54e878811
Neerav Panchal <neeravpanchal26@gmail.com>
2024-08-03 15:14:13 -0400

    [03-08-24] Created missing xaml.cs file

---
+++ CMP1701 - Cleaning App.Droid/ViewModel/CleaningSchedulePage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Maui.Controls;
+using CMP1701___Cleaning_App.Droid.Models;
+
+namespace CMP1701___Cleaning_App
+{
+    public partial class CleaningDevicePage : ContentPage
+    {
+        private CleaningDevice _cleaningDevice;
+
+        public CleaningDevicePage(CleaningDevice cleaningDevice)
+        {
+            InitializeComponent();
+            _cleaningDevice = cleaningDevice;
+            PopulateDeviceDetails();
+        }
+
+        private void PopulateDeviceDetails()
+        {
+            if (_cleaningDevice == null)
+                return;
+
+            DeviceIdLabel.Text = _cleaningDevice.DeviceId.ToString();
+            DeviceNameLabel.Text = _cleaningDevice.DeviceName;
+            DeviceTypeLabel.Text = _cleaningDevice.DeviceType;
+            BatteryLevelLabel.Text = $"{_cleaningDevice.BatteryLevel}%";
+            DeviceStatusLabel.Text = _cleaningDevice.DeviceStatus;
+            RoomAssignedLabel.Text = _cleaningDevice.RoomAssigned;
+        }
+    }
+}
